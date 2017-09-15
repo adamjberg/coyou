b31274c6abeb3bb03c7f46bf6f11f6eb5e4297f9
Adam Berg <adam@xyzdigital.com>
2017-09-14 17:14:39 -0700

    Remove models sub dir

--- models/definitions/User.d.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose';
-export declare const UserSchema: any;
-export interface IUser {
-    email?: string;
-    password?: string;
-}
-export interface IUserDocument extends IUser, mongoose.Document {
-}
-export declare const User: any;
