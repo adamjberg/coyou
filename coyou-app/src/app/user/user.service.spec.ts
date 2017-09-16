d19b60e637998d59aa5eb2beca64e18219f6ba83
Adam Berg <adam@xyzdigital.com>
2017-09-16 14:50:46 -0700

    Add travis yml

--- coyou-app/src/app/user/user.service.spec.ts
+++ coyou-app/src/app/user/user.service.spec.ts
@@ -4,19 +4,27 @@ import { TestBed, inject } from '@angular/core/testing';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        UserService,
-        {
-          provide: HttpClient,
-          useValue: {}
-        }
-      ]
+  describe('unit', () => {
+    it('should not load testbed', () => {
+
     });
   });
 
-  it('should be created', inject([UserService], (service: UserService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('testbed', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          UserService,
+          {
+            provide: HttpClient,
+            useValue: {}
+          }
+        ]
+      });
+    });
+
+    it('should be created', inject([UserService], (service: UserService) => {
+      expect(service).toBeTruthy();
+    }));
+  });
 });
